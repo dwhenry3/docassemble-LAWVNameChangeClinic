2910b93ebf9a974bf1ecd462de826c4f9de48080
System Administrator <127511792+dwhenry3@users.noreply.github.com>
2025-05-02 10:12:16 -0400

    fix issue with county being referenced before geocoding address, added call to LAWVCommon for court preference instead of internal reference

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.LAWVNameChangeClinic',
-      version='1.0.6',
+      version='1.0.7',
       description=('A docassemble extension.'),
       long_description='A guided interview for creating an adult Name Change Petition Packet for a LAWV Name Change Clinic. The interview assumes that it is going to be completed by an attorney or paralegal assisting in the clinic.  The interview produces PDF and Word documents to allow for final edits.',
       long_description_content_type='text/markdown',
@@ -53,7 +53,7 @@ setup(name='docassemble.LAWVNameChangeClinic',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
       packages=find_namespace_packages(),
-      install_requires=['docassemble.LAWVCommon>=1.0.12'],
+      install_requires=['docassemble.LAWVCommon>=1.0.13'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/LAWVNameChangeClinic/', package='docassemble.LAWVNameChangeClinic'),
      )
